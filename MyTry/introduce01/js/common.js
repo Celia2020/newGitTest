190355bca28bb06caae19af32531cf1b0cf03404
Shen <li.shen@rakuten.com>
2020-06-17 17:28:01 +0800

    finish percent page's template of svg & javascript

--- MyTry/introduce01/js/common.js
+++ MyTry/introduce01/js/common.js
@@ -1,12 +1,22 @@
 // common file（ header nav ）
 
-var OpenMenu = document.querySelectorAll('.OpenMenu');
+var OpenMenu = document.querySelectorAll(".OpenMenu");
 OpenMenu.forEach((OpenMenu) =>
   OpenMenu.addEventListener("click", (e) => {
-    document.querySelector('.mainmenu').classList.add('show');
-  }));
+    document.querySelector(".mainmenu").classList.add("show");
+  })
+);
 
-var CloseMenu = document.querySelector('.closebtn');
-CloseMenu.addEventListener("click",(e) =>{
-  document.querySelector('.mainmenu').classList.remove('show');
-});
+var CloseMenu = document.querySelectorAll(".close");
+CloseMenu.forEach((closebtn) =>
+  closebtn.addEventListener("click", (e) => {
+    document.querySelector(".mainmenu").classList.remove("show");
+  })
+);
+
+var CloseMenu = document.querySelectorAll(".closeself");
+CloseMenu.forEach((closeself) =>
+  closeself.addEventListener("click", (e) => {
+    document.querySelector(".closeself").style.display = "none";
+  })
+);
--- MyTry/introduce01/js/pctlst.js
+++ MyTry/introduce01/js/pctlst.js
@@ -1,49 +1,59 @@
 // knowledge percent list //
 //
-var SectionArr = document.querySelectorAll('.mainarea section');
+var SectionArr = document.querySelectorAll(".mainarea section");
 // console.log(SectionArr[0].getAttribute('id'));   >>> example
 
 SectionArr.forEach(myfuc);
 
-function myfuc(e){
+function myfuc(e) {
   // e = SectionArr
-  var IDarray = e.getAttribute('id');
-  var Ppct = document.querySelector('#' + IDarray + ' .Ppct span').innerText;
-  var UFpct = document.querySelector('#' + IDarray + ' .UFpct span').innerText;
+  var IDarray = e.getAttribute("id");
+  var Ppct = document.querySelector("#" + IDarray + " .Ppct span").innerText;
+  var UFpct = document.querySelector("#" + IDarray + " .UFpct span").innerText;
 
-  // Proficiency width control
-  var SpanWidthPpct = document.querySelector('#' + IDarray + ' .Ppct span').style.width = `${Ppct}%`;
-  var SpanMarginLftPpct = document.querySelector('#' + IDarray + ' .Ppct span').style.marginLeft = `${100 - Ppct}%`;
+  // Proficiency width & marginLeft control
+  document.querySelector(
+    "#" + IDarray + " .Ppct span"
+  ).style.width = `${Ppct}%`;
+  document.querySelector(
+    "#" + IDarray + " .Ppct span"
+  ).style.marginLeft = `${100 - Ppct}%`;
 
-  // UsageFrequency width control
-  var SpanWidthUFpct = document.querySelector('#' + IDarray + ' .UFpct span').style.width = `${UFpct}%`;
-  var SpanMarginRightUFpct = document.querySelector('#' + IDarray + ' .UFpct span').style.marginRight = `${100 - UFpct}%`;
+  // UsageFrequency width & marginLeft control
+  document.querySelector(
+    "#" + IDarray + " .UFpct span"
+  ).style.width = `${UFpct}%`;
+  document.querySelector(
+    "#" + IDarray + " .UFpct span"
+  ).style.marginRight = `${100 - UFpct}%`;
 }
-
 
 // floating layer show //
 //
 var btnMores = document.querySelectorAll(".more");
 // console.log(btnMores);
 btnMores.forEach((btnMore) =>
-  btnMore.addEventListener("click", (e) =>{
+  btnMore.addEventListener("click", (e) => {
     // console.log(btnMore);
     // console.log(e);
-    let btnMorebyID = btnMore.parentNode.parentNode.parentNode.parentNode.getAttribute('id');
+    let btnMorebyID = btnMore.parentNode.parentNode.parentNode.parentNode.getAttribute(
+      "id"
+    );
     // console.log(btnMorebyID);
-    document.querySelector('#' + btnMorebyID + ' .fltlyr').style.display = "block"; 
-}));
-
-
+    document.querySelector("#" + btnMorebyID + " .fltlyr").style.display =
+      "block";
+  })
+);
 
 // floating layer close start //
 //
-var CloseMenus = document.querySelectorAll('.closebtn');
-// console.log(CloseMenus);
+var CloseMenus = document.querySelectorAll(".closebtn");
 CloseMenus.forEach((CloseMenu) =>
-  CloseMenu.addEventListener("click", (e) =>{
-    // console.log(CloseMenu);
-    let btnClosebyID = CloseMenu.parentNode.parentNode.parentNode.getAttribute('id');
-    // console.log(btnClosebyID);
-    document.querySelector('#' + btnClosebyID + ' .fltlyr').style.display = "none";
-}));
+  CloseMenu.addEventListener("click", (e) => {
+    let btnClosebyID = CloseMenu.parentNode.parentNode.parentNode.getAttribute(
+      "id"
+    );
+    document.querySelector("#" + btnClosebyID + " .fltlyr").style.display =
+      "none";
+  })
+);
