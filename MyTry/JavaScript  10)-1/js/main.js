c19aab0a2c2c10e9283d27cf99a49887444f8f4f
Shen <li.shen@rakuten.com>
2020-07-28 13:42:09 +0800

    JavaScript   10)用JavaScript實現動畫效果-1

--- MyTry/JavaScript  10)-1/js/main.js
+++ MyTry/JavaScript  10)-1/js/main.js
@@ -19,7 +19,19 @@ function positionMessage(){
   elem.style.position = "absolute";
   elem.style.left = "50px";
   elem.style.top = "100px";
-  movement = setTimeout("moveMessage()",5000);
+  movement = setTimeout("moveMessage()",1000);
+
+  if(!document.getElementById("message2")) return false;
+  var elem2 = document.getElementById("message2");
+  elem2.style.position = "absolute";
+  elem2.style.left = "50px";
+  elem2.style.top = "100px";
+  moveElement("message2",200,100,10);
+
+console.log(parseFloat("39.89j90"));
+console.log(parseFloat("39.8990"));
+console.log(parseFloat("39.0"));
+
 }
 
 function moveMessage(){
@@ -44,6 +56,36 @@ function moveMessage(){
   if(Ypos > 100){
     Ypos--;
   }
+  elem.style.left = Xpos + "px";
+  elem.style.top = Ypos + "px";
+  movement = setTimeout("moveMessage()",10);
+}
+
+function moveElement(elementID,final_x,final_y,interval){
+  if(!document.getElementById) return false;
+  if(!document.getElementById(elementID)) return false;
+  var elem2 = document.getElementById(elementID);
+  var Xpos = parseInt(elem2.style.left);
+  var Ypos = parseInt(elem2.style.top);
+  if(Xpos == final_x && Ypos == final_y){
+    return true;
+  }
+  if(Xpos < final_x){
+    Xpos++;
+  }
+  if(Xpos > final_x){
+    Xpos--;
+  }
+  if(Ypos < final_y){
+    Ypos++;
+  }
+  if(Ypos > final_y){
+    Ypos--;
+  }
+  elem2.style.left = Xpos + "px";
+  elem2.style.top = Ypos + "px";
+  var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+  movement = setTimeout(repeat, interval);
 }
 
 // Final: add all functions
